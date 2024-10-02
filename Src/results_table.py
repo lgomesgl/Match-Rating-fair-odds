ecf5dcee91b7f03b4a349289c248999cc22013b0
EnzoFarias433 <enzofarias34@hotmail.com>
2024-10-02 12:31:12 -0300

    comentarios

--- Src/results_table.py
+++ Src/results_table.py
@@ -4,9 +4,23 @@ import pandas as pd
 class Table:
 
     def __init__(self, league):
+        """ 
+        table class sorted by points in each match
+        each table class consists of a table dictionary in which teams are the keys and the values are they're current 
+        points in the league, table league consists of the league name which is being analyzed
+        
+        win : +3 points
+        draw: +1 points for both teams
+        loss: no points
+
+        in case two teams are tied in points tie breaker parameters are decided in the following order:
+
+        goals > goals taken        
+        """
         self.league = league
         self.table = {}
 
+    #create the table based on .csv given
     def table(self, path):
         dados = pd.read_csv(path)
         for i in range(1, len(dados)):
