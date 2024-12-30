c263ceb3326ef884d7897de1619d6f6cd119d405
lgomesgl <lgomes855@gmail.com>
2024-12-30 10:58:16 -0300

    put the restrictions in the tickets

--- src/simulations/simulation.py
+++ src/simulations/simulation.py
@@ -4,6 +4,7 @@ import pandas as pd
 import os
 from typing import List, Dict
 from concurrent.futures import ProcessPoolExecutor
+from collections import Counter
 from src.utils.tools import load_json_file
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
@@ -38,6 +39,7 @@ class Simulation:
             Simulate matchs using monte carlo and combine them in a ticket
         """
         self.matchs = matches
+        self.best_tickets = []
 
     def extract_games_by_league(self, leagues_name: List[str] = ['Premier League', 'La Liga', 'Bundesliga', 'Serie A', 'Ligue 1']):
         games = []
@@ -53,10 +55,35 @@ class Simulation:
 
         return games  
     
-    def _restictions(self):
-        pass
-    
-    def _get_probabilities(self, tickets: np.ndarray) -> np.ndarray:
+    @staticmethod
+    def _restrictions(ticket: List[Dict[str, str]], restriction_league: bool) -> bool:
+        """
+        Apply restrictions to the ticket.
+        """
+        league_count = Counter(game['league'] for game in ticket)
+        result_count = Counter(game['result'] for game in ticket)
+        ticket_size = len(ticket)
+
+        # Restriction 1: No league should have more than 2 games
+        if restriction_league:
+            if max(league_count.values(), default=0) > 2:
+                return False
+        
+        # Restriction 2: No result should be only in a ticket
+        if max(result_count.values(), default=0) == ticket_size:
+            return False
+        
+        # Restriction 3: No more them 40% draw games in a ticket
+        if result_count['Draw'] > 0.40 * ticket_size:
+            return False
+        
+        # Restriction 4: No more them 40% away games in a ticket
+        if result_count['Away'] > 0.40 * ticket_size:
+            return False
+
+        return True
+
+    def _get_probabilities(self, tickets: np.ndarray, restriction_league: bool = True) -> np.ndarray:
         """
             Receive the tickets with the games and get the probabilities of each game.
             Apply the restrictions
@@ -66,22 +93,53 @@ class Simulation:
 
         tickets_probabilities = []
         for ticket in tickets:
-            #print(ticket)
             ticket_probabilities = []
             for game in ticket:
-                #print(game)
                 prob_choice = rng.choice(results, p=game['probabilities'])
 
                 ticket_probabilities.append({"league": game["league"], 
                                               "match": game["match"],
+                                              "probabilities": game["probabilities"],
                                               "result": prob_choice})
             
-            #if self._restictions():
-            tickets_probabilities.append(ticket_probabilities)
+            if self._restrictions(ticket=ticket_probabilities, restriction_league=restriction_league):
+                tickets_probabilities.append(ticket_probabilities)
         
         return tickets_probabilities
 
+    def _get_best_tickets(self, tickets: List[List[Dict]], top_n: int = 5) -> List[List[Dict]]:
+        """
+        Select the best tickets based on the product of probabilities for the chosen results.
 
+        Args:
+            tickets: A list of tickets where each ticket is a list of game dictionaries.
+            top_n: The number of top tickets to return.
+
+        Returns:
+            A list of the top N tickets based on their total probabilities.
+        """
+        scored_tickets = []
+
+        for ticket in tickets:
+            ticket_score = 1.0  
+            for game in ticket:
+                chosen_prob = game['probabilities'][['Home', 'Draw', 'Away'].index(game['result'])]
+                ticket_score *= chosen_prob
+
+            scored_tickets.append((ticket, ticket_score))
+
+        scored_tickets.sort(key=lambda x: x[1], reverse=True)
+        self.best_tickets = [ticket for ticket, _ in scored_tickets[:top_n]]
+
+        return self.best_tickets
+
+    def show_tickets(self):
+        print("Best Tickets:")
+        for i, ticket in enumerate(self.best_tickets, 1):
+            print(f"Ticket {i}:")
+            for game in ticket:
+                print(f"  {game}")
+        
     def run():
         pass
 
@@ -92,5 +150,7 @@ if __name__ == '__main__':
     sl = Simulation(matches=games)
     g = sl.extract_games_by_league()
     mc = MonteCarlo(games=g)
-    ga = mc.simulate(100, 5)
-    ga_ = sl._get_probabilities(tickets=ga)
+    ga = mc.simulate(100000, 5)
+    ga_ = sl._get_probabilities(tickets=ga, restriction_league=True)
+    best = sl._get_best_tickets(tickets=ga_)
+    sl.best_tickets()
