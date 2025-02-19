3351ae89ed168d85c66f339829d31ce18168416b
lgomesgl <lgomes855@gmail.com>
2025-02-18 22:39:45 -0300

    Classification table for weights False

--- src/main.py
+++ src/main.py
@@ -33,7 +33,7 @@ def main(league_name, match_rating_path):
         for stats in ['Gols', 'Shoots', 'Target Shoots']:
             match_rat = MatchRating(league_name=league_name, matchs_rating=matchs_rating, statistic=stats, gols=1.5)
             match_rat.get_columns()
-            match_rat.get_match_rating(data=df, file_name=data, classification=True) 
+            match_rat.get_match_rating(data=df, file_name=data, classification=False) 
         count += 1
         logging.info(f'Datasets training: { count }/8')
     
